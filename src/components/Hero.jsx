2af3bc6058277b868464ac781eecbfed9c122b1e
Emerson <jobbr70@gmail.com>
2025-04-24 13:47:38 -0300

    Feat: install framer-motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { FaDownload, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import Profile from '../assets/profile.png'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
-    <section className='h-max md:h-screen bg-gradient-to-l bg-gray-950 text-white flex flex-col justify-center relative z-10 pb-10 '>
+    <section className='h-max md:h-screen bg-gradient-to-l bg-gray-950 text-white flex flex-col justify-start relative z-10 pb-10 '>
       <div className='max-w-7xl mt-24 mx-auto items-center flex flex-col md:flex-row gap-16 md:gap-40 justify-between'>
         <div className='md:space-y-6 px-4'>
-          <h1 className='md:text-6xl px-4 font-bold mb-4 ' >Hi, I'm <p className='text-blue-400'>Emerson</p>Leme  </h1>
+          <motion.h1
+          initial={{opacity:0, x:-100}}
+          whileInView={{opacity:1, x:0}}
+          transition={{duration:1, delay:0.2}}          
+          
+          className='md:text-6xl text-4xl font-bold mb-4 ' >Hi, I'm <p className='text-blue-400'>Emerson</p>Leme  </motion.h1>
+          
           <p className='md:text-2xl text-lg mb-3'>Full-Stack Developer</p>
           <p className='lg:w-[600px] mb-4 md:mb-0 text-sm text-gray-300 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et laboriosam iste ipsa dolores? Voluptas et quae eveniet obcaecati voluptatibus eaque dolorem fugiat labore cumque esse. </p>
           <button className='md:mt-3 px-3 py-2 flex items-center gap-2 bg-blue-500 rounded-md text-white hover:bg-blue-600 shadow-blue-500 border-2 border-blue-400 transition-all shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC]'><FaDownload>
@@ -17,9 +25,14 @@ const Hero = () => {
              <FaLinkedin className='hover:text-blue-400'/>
              <FaFacebook  className='hover:text-blue-400'/>
             </div>
+        </div>
+        <div className='relative group:'>
+          <img src={Profile} alt='' className=' rounded-full border border-blue-600 md:w-[300px] w-[300px] shadow-[0px_0px_20px_10px_rgba(0,0,0,0.3)] shadow-blue-500'/>
 
 
         </div>
+
+
       </div>
     
     </section>
